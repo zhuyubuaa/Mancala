42151aee31f73576428a374604578df2d88cfeb3
root <root@LAPTOP-7S9QB5KP>
2024-03-26 22:14:23 +0800

    pass final test

--- T1/t1_rust/src/lib.rs
+++ T1/t1_rust/src/lib.rs
@@ -10,21 +10,25 @@ pub fn bocchi_shut_up(flag: i32, seq: &[i32], size: i32) -> i32 {
         }
     }
 
-    let mut most_frq = 0;
+    let mut most_frq = -1;
     let mut same : bool = false;
+    let mut index = -1;
+    let mut cnt = 0;
     for el in table {
         if el > most_frq{
             same = false;
             most_frq = el;
+            index = cnt;
         } else if el == most_frq {
             same = true;
         }
+        cnt = cnt + 1;
     }
 
     if same {
         10
     } else {
-        flag * 10 + most_frq
+        flag * 10 + index
     }
 }
 
