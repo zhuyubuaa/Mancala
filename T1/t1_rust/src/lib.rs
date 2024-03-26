e153b2a748158ed3226b6cbdf5e44ca54a8793e4
root <root@LAPTOP-7S9QB5KP>
2024-03-26 21:51:55 +0800

    create pkg & copy code v1

--- T1/t1_rust/src/lib.rs
+++ T1/t1_rust/src/lib.rs
@@ -2,7 +2,29 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn bocchi_shut_up(flag: i32, seq: &[i32], size: i32) -> i32 {
-    10
+    let mut table = [0; 7];
+    for el in seq {
+        if el / 10 == flag {
+            table[el % 10] = table[el % 10] + 1;
+        }
+    }
+
+    let mut most_frq = 0;
+    let mut same : bool = false;
+    for el in table {
+        if el > most_frq{
+            same = false;
+            most_frq = el;
+        } else if el == most_frq {
+            same = true;
+        }
+    }
+
+    if same {
+        10
+    } else {
+        flag * 10 + most_frq
+    }
 }
 
 #[cfg(test)]
